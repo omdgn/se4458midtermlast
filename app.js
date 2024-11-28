ce0c1cdeaac0e3629bafb4ec6ba55c2e32ec326b
zak6499 <omdg_n@outlook.com>
2024-11-29 00:10:27 +0300

    add env file

--- app.js
+++ app.js
@@ -20,5 +20,5 @@ app.use('/api/auth', authRoutes);
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
